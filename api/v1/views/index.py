bce1b679d770b039a68dfa8069fb111d9842937c
luckys-lnz <lalucky22@gmail.com>
2024-08-01 23:28:31 +0000

    Data not populated from DB

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,29 @@
+#!/usr/bin/python3
+"""
+Create an endpoint that retrieves the number
+of each objects by type:
+"""
+
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
+
 
 @app_views.route('/status', methods=['GET'])
 def get_status():
-    """ Returns the API status """
+    """Check status of file"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'])
+def get_stats():
+    """Create an endpoint"""
+    statistics = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User"),
+    }
+    return jsonify(statistics)
