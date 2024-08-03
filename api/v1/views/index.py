27185e74cb74f7601fff6ca92a2a817d68334600
deantosh <deantoshdaiddoh@gmail.com>
2024-08-03 21:49:58 +0000

    route: /stats added

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,6 +7,12 @@ of each objects by type:
 from flask import jsonify
 from api.v1.views import app_views
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.state import State
+from models.place import Place
+from models.user import User
+from models.review import Review
 
 
 @app_views.route('/status', methods=['GET'])
@@ -19,11 +25,11 @@ def get_status():
 def get_stats():
     """Create an endpoint"""
     statistics = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User"),
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User),
     }
     return jsonify(statistics)
