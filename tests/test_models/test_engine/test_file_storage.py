c7e35271eab6719668964d54018b8ba6379c0e5a
deantosh <deantoshdaiddoh@gmail.com>
2024-08-03 17:20:50 +0000

    get() method test: added

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -70,6 +70,13 @@ test_file_storage.py'])
 
 class TestFileStorage(unittest.TestCase):
     """Test the FileStorage class"""
+    def tearDown(self):
+        """Cleans up  test environment"""
+        try:
+            os.remove("file.json")
+        except FileNotFoundError:
+            pass
+
     @unittest.skipIf(models.storage_t == 'db', "not testing file storage")
     def test_all_returns_dict(self):
         """Test that all returns the FileStorage.__objects attr"""
