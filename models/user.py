add9c20dc0c6d675a3ee5a116219d7180156a6b5
deantosh <deantoshdaiddoh@gmail.com>
2024-08-05 08:28:49 +0000

    models

--- models/user.py
+++ models/user.py
@@ -1,29 +1,32 @@
 #!/usr/bin/python3
-""" holds class User"""
-import models
+"""This module defines a class User"""
+import os
 from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
+# Get storage type
+storage_type = os.getenv("HBNB_TYPE_STORAGE")
+
+
 class User(BaseModel, Base):
-    """Representation of a user """
-    if models.storage_t == 'db':
+    """This class defines a user by various attributes"""
+
+    if storage_type == "db":
+        # Handle database storage
         __tablename__ = 'users'
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
-        places = relationship("Place", backref="user")
-        reviews = relationship("Review", backref="user")
+        places = relationship("Place", backref="user",
+                              cascade="all, delete-orphan")
+        reviews = relationship("Review", backref="user",
+                               cascade="all, delete-orphan")
     else:
+        # Handle file storage
         email = ""
         password = ""
         first_name = ""
         last_name = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes user"""
-        super().__init__(*args, **kwargs)
