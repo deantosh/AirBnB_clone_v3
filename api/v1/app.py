d67093641518569f0e13e8817de310e27edfc637
luckys-lnz <lalucky22@gmail.com>
2024-08-05 10:26:25 +0000

    12. HTTP access control (CORS)

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,6 +4,7 @@ Module defines a Flask application
 """
 
 from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 import os
@@ -11,6 +12,7 @@ import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
 
 
 @app.errorhandler(404)
